9f14f38864f171feac3df423d43bf97c3aa63526
Khuda Dad Nomani <32505158+KhudaDad414@users.noreply.github.com>
2021-03-26 09:36:16 +0530

    extra spaces removed.

--- template/src/lib/message-validator.js
+++ template/src/lib/message-validator.js
@@ -3,5 +3,4 @@ const AsyncApiValidator = require('asyncapi-validator');
 module.exports.validateMessage = async (payload, topic,name,operation)=> {
   let va = await AsyncApiValidator.fromSource('./asyncapi.yaml', {msgIdentifier: 'name'});
   va.validate(name, payload, topic, operation);
-
 }
