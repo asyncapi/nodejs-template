bd5a3042ad6c25f031ab69e786181ccdfb4a26ae
khudadad414 <khudadad414@gmail.com>
2021-03-26 09:18:44 +0530

    integrate asyncapi-validator

---
+++ template/src/lib/message-validator.js
@@ -0,0 +1,7 @@
+const AsyncApiValidator = require('asyncapi-validator');
+
+module.exports.validateMessage = async (payload, topic,name,operation)=> {
+  let va = await AsyncApiValidator.fromSource('./asyncapi.yaml', {msgIdentifier: 'name'});
+  va.validate(name, payload, topic, operation);
+
+}
