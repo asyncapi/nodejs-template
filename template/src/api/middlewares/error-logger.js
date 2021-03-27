1bc087c6aa3c69d29d40316c411c92c74020b4e8
Khuda Dad Nomani <32505158+KhudaDad414@users.noreply.github.com>
2021-03-27 17:26:17 +0530

    indent corrected.

--- template/src/api/middlewares/error-logger.js
+++ template/src/api/middlewares/error-logger.js
@@ -1,9 +1,10 @@
 const { red, gray } = require('colors/safe');
 
 module.exports = (err, message, next) => {
-  if(err.name === 'AsyncAPIValidationError'){
+  if (err.name === 'AsyncAPIValidationError'){
     console.error(red(`❗  Message Rejected. ${err.message}`));
-  } else {
+  } 
+  else {
     console.error(red(`❗  ${err.message}`));
     if (err.stack) console.error(gray(err.stack.substr(err.stack.indexOf('\n')+1)));
   }
