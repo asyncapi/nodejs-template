5191bfe19603416dd6969fc071805127caad9bf5
David Biesack <David.Biesack@apiture.com>
2021-08-05 17:17:49 -0400

    Updated `$$channel$$.js` (and `lib/message-validator.js`) to support `oneOf:` messages
    Modified the `validateMessage` function to validate the payload and if
    valid, increment and return a counter of validated messages. Ignore
    validation exceptions. Throw an exception only if `nValidated > 1` (no
    need to try validating more - fail fast). `router` functions (publish
    or subscribe) try to validate the payload against all of the messages.
    If exactly one matches, then we proceed, else we stop.

--- template/src/lib/message-validator.js
+++ template/src/lib/message-validator.js
@@ -1,6 +1,14 @@
 const AsyncApiValidator = require('asyncapi-validator');
 
-module.exports.validateMessage = async (payload, channel,name,operation)=> {
-  let va = await AsyncApiValidator.fromSource('./asyncapi.yaml', {msgIdentifier: 'name'});
-  va.validate(name, payload, channel, operation);
+module.exports.validateMessage = async (payload, channelName, messageName, operation, nValidated) => {
+  try {
+    let va = await AsyncApiValidator.fromSource('./asyncapi.yaml', {msgIdentifier: 'name'});
+    va.validate(messageName, payload, channelName, operation);
+    nValidated++;
+  } catch (e) {
+  }
+  if (nValidated > 1) {
+    throw new Error(`At least ${nValidated} message schema matched when exactly 1 should match`);
+  }
+  return nValdated;
 }
