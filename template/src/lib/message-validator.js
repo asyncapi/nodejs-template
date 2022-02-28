466c024478cef92d2e9d44908121959858722666
David Biesack <David.Biesack@apiture.com>
2022-02-28 08:41:35 -0500

    remove throw from inside validateMessage; let caller throw

--- template/src/lib/message-validator.js
+++ template/src/lib/message-validator.js
@@ -1,14 +1,12 @@
 const AsyncApiValidator = require('asyncapi-validator');
 
-module.exports.validateMessage = async (payload, channelName, messageName, operation, nValidated) => {
+// Try to parse the payload, and increment nValidated if parsing was successful.
+module.exports.validateMessage = async (payload, channelName, messageName, operation, nValidated=0) => {
   try {
     let va = await AsyncApiValidator.fromSource('./asyncapi.yaml', {msgIdentifier: 'name'});
     va.validate(messageName, payload, channelName, operation);
     nValidated++;
   } catch (e) {
-  }
-  if (nValidated > 1) {
-    throw new Error(`At least ${nValidated} message schema matched when exactly 1 should match`);
   }
   return nValidated;
 }
