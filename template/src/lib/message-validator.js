c346299118975a8ba23ab4dd5178547c78b0060b
David Biesack <David.Biesack@apiture.com>
2022-02-28 09:36:55 -0500

    use let instead of const

--- template/src/lib/message-validator.js
+++ template/src/lib/message-validator.js
@@ -3,7 +3,7 @@ const AsyncApiValidator = require('asyncapi-validator');
 // Try to parse the payload, and increment nValidated if parsing was successful.
 module.exports.validateMessage = async (payload, channelName, messageName, operation, nValidated=0) => {
   try {
-    let va = await AsyncApiValidator.fromSource('./asyncapi.yaml', {msgIdentifier: 'name'});
+    const va = await AsyncApiValidator.fromSource('./asyncapi.yaml', {msgIdentifier: 'name'});
     va.validate(messageName, payload, channelName, operation);
     nValidated++;
   } catch (e) {
