9d0c57507ed6dd7ff2466ab0e1dc317c8945cb4f
Kaushik Rishi Manchukonda <rishi.cp01@gmail.com>
2023-08-15 19:59:12 +0530

    feat: refactoring handlers to separate from generated code

--- template/src/api/handlers/$$channel$$.js
+++ template/src/api/handlers/$$channel$$.js
@@ -1,21 +1,13 @@
 {%- if channel.hasPublish() and channel.publish().ext('x-lambda') %}const fetch = require('node-fetch');{%- endif %}
 const handler = module.exports = {};
+
+const middlewares = [];
+
+handler.registerMiddleware = (middlewareFn) => {
+  middlewares.push(middlewareFn);
+}
+
 {% if channel.hasPublish() %}
-/**
- * {{ channel.publish().summary() }}
- * @param {object} options
- * @param {object} options.message
-{%- if channel.publish().message(0).headers() %}
-{%- for fieldName, field in channel.publish().message(0).headers().properties() %}
-{{ field | docline(fieldName, 'options.message.headers') }}
-{%- endfor %}
-{%- endif %}
-{%- if channel.publish().message(0).payload() %}
-{%- for fieldName, field in channel.publish().message(0).payload().properties() %}
-{{ field | docline(fieldName, 'options.message.payload') }}
-{%- endfor %}
-{%- endif %}
- */
 handler.{{ channel.publish().id() }} = async ({message}) => {
   {%- if channel.publish().ext('x-lambda') %}
   {%- set lambda = channel.publish().ext('x-lambda') %}
@@ -30,29 +22,18 @@ handler.{{ channel.publish().id() }} = async ({message}) => {
     .then(json => console.log(json))
     .catch(err => { throw err; });
   {%-  else %}
-  // Implement your business logic here...
+  for (const middleware of middlewares) {
+    await middleware(message);
+  }
   {%- endif %}
 };
 
 {%- endif %}
 {%- if channel.hasSubscribe() %}
-/**
- * {{ channel.subscribe().summary() }}
- * @param {object} options
- * @param {object} options.message
-{%- if channel.subscribe().message(0).headers() %}
-{%- for fieldName, field in channel.subscribe().message(0).headers().properties() %}
-{{ field | docline(fieldName, 'options.message.headers') }}
-{%- endfor %}
-{%- endif %}
-{%- if channel.subscribe().message(0).payload() %}
-{%- for fieldName, field in channel.subscribe().message(0).payload().properties() %}
-{{ field | docline(fieldName, 'options.message.payload') }}
-{%- endfor %}
-{%- endif %}
- */
 handler.{{ channel.subscribe().id() }} = async ({message}) => {
-  // Implement your business logic here...
+  for (const middleware of middlewares) {
+    await middleware(message);
+  }
 };
 
 {%- endif %}
