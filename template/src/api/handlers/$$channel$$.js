91ac6934fb57e264c9286b760a05de9083b2545f
Kaushik Rishi Manchukonda <rishi.cp01@gmail.com>
2023-08-25 07:43:17 +0530

    update jsdoc

--- template/src/api/handlers/$$channel$$.js
+++ template/src/api/handlers/$$channel$$.js
@@ -3,11 +3,38 @@ const handler = module.exports = {};
 
 const middlewares = [];
 
+/**
+ * Registers a middleware function to be executed during request processing.
+ *
+ * Middleware functions have access to options object that you can use to access the message content and other helper functions
+ *
+ * @param {function} middlewareFn - The middleware function to be registered.
+ * @throws {TypeError} If middlewareFn is not a function.
+ */
 handler.registerMiddleware = (middlewareFn) => {
+  if (typeof middlewareFn !== 'function') {
+    throw new TypeError('middlewareFn must be a function');
+  }
   middlewares.push(middlewareFn);
 }
 
 {% if channel.hasPublish() %}
+/**
+ * {{ channel.publish().summary() }}
+ *
+ * @param {object} options
+ * @param {object} options.message
+{%- if channel.publish().message(0).headers() %}
+{%- for fieldName, field in channel.publish().message(0).headers().properties() %}
+{{ field | docline(fieldName, 'options.message.headers') }}
+{%- endfor %}
+{%- endif %}
+{%- if channel.publish().message(0).payload() %}
+{%- for fieldName, field in channel.publish().message(0).payload().properties() %}
+{{ field | docline(fieldName, 'options.message.payload') }}
+{%- endfor %}
+{%- endif %}
+ */
 handler.{{ channel.publish().id() }} = async ({message}) => {
   {%- if channel.publish().ext('x-lambda') %}
   {%- set lambda = channel.publish().ext('x-lambda') %}
@@ -29,7 +56,24 @@ handler.{{ channel.publish().id() }} = async ({message}) => {
 };
 
 {%- endif %}
+
 {%- if channel.hasSubscribe() %}
+/**
+ * {{ channel.subscribe().summary() }}
+ *
+ * @param {object} options
+ * @param {object} options.message
+ {%- if channel.subscribe().message(0).headers() %}
+ {%- for fieldName, field in channel.subscribe().message(0).headers().properties() %}
+ {{ field | docline(fieldName, 'options.message.headers') }}
+ {%- endfor %}
+ {%- endif %}
+ {%- if channel.subscribe().message(0).payload() %}
+ {%- for fieldName, field in channel.subscribe().message(0).payload().properties() %}
+ {{ field | docline(fieldName, 'options.message.payload') }}
+ {%- endfor %}
+ {%- endif %}
+ */
 handler.{{ channel.subscribe().id() }} = async ({message}) => {
   for (const middleware of middlewares) {
     await middleware(message);
