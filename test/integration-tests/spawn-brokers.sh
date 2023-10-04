def019831fad96f358401410202ae8bc1ad83b59
Kaushik Rishi Manchukonda <rishi.cp01@gmail.com>
2023-10-04 21:57:46 +0530

    simplify integration test folder structure single layer

--- test/integration-tests/spawn-brokers.sh
+++
@@ -1,4 +0,0 @@
-# Include docker commands to spawn docker containers for brokers (amqp, mqtt, kafka, etc ...)
-# Internally this will be run before running the integration tests
-
-docker run -it -p 1883:1883 eclipse-mosquitto:1.5
